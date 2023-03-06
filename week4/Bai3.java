3a5d0f579dc34ed5ed1a92244bc8d4c6b0abe450
nguyenhoang213 <hoang23032003@gmail.com>
2023-03-06 21:58:01 +0700

    tuan 4

--- week4/Bai3.java
+++ week4/Bai3.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Bai3 {
     public static void main(String[] args) {
         String name;
-        int age;
+        int year;
         Scanner input = new Scanner (System.in);
         System.out.println("Nhap Ten:");
         name = input.nextLine();
-        System.out.println("Nhap Tuoi:");
-        age = input.nextInt();
-        if(age < 16) System.out.println("Ban " + name + " o do tuoi vi thanh nien");
-        else if(age < 18) System.out.println("Ban " + name + " o do tuoi truong thanh");
+        System.out.println("Nhap Nam Sinh:");
+        year = input.nextInt();
+        if(2023 - year  < 16) System.out.println("Ban " + name + " o do tuoi vi thanh nien");
+        else if(2023 - year < 18) System.out.println("Ban " + name + " o do tuoi truong thanh");
         else System.out.println("Ban " + name + " da gia");
     }
 }
