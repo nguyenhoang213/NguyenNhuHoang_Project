a893be603b392b1b77c6d613abf99a49d55bcc0b
nguyenhoang213 <hoang23032003@gmail.com>
2023-03-13 16:24:32 +0700

    tuan 5

---
+++ week5/Bai6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Bai6 {
+    public static void main(String[] args) {
+        int n, gt = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap 1 so nguyen duong: ");
+        n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            gt *= i;
+        }
+        System.out.println("Giai thua cua so da nhap la " + gt);
+    }
+}
